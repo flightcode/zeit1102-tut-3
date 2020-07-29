59fe25d66032cbe03b5ea604ee501b57b2e08c2a
Elliot Parker <elliotccp@protonmail.com>
2020-07-29 17:17:38 +1000

    Add Car

---
+++ Car.java
@@ -0,0 +1,40 @@
+public class Car {
+
+    String rego;
+    float speed;
+    String colour;
+
+    public static void main(String[] args) {
+        // Do stuff
+    }
+
+    void setCar(String rego, float speed, String colour) {
+    }
+
+    void setRego(String rego) {
+    }
+
+    void setSpeed(float speed) {
+    }
+
+    void setColour(String colour) {
+    }
+
+    String getRego() {
+    }
+
+    float getSpeed() {
+    }
+
+    String getColour() {
+    }
+
+    void accelerate(float speed) {
+    }
+
+    void brake(float speed) {
+    }
+
+    String toString() {
+    }
+}
